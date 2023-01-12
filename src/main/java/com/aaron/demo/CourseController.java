2d57d14ec952a1737c7f9bf8196058963d2e69ea
Aaron Spence <aaronspence31@gmail.com>
2023-01-12 15:46:26 -0500

    configuration properties

--- src/main/java/com/aaron/demo/CourseController.java
+++ src/main/java/com/aaron/demo/CourseController.java
@@ -1,22 +1,16 @@
 package com.aaron.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 
 @RestController
 public class CourseController {
-    //http://localhost:8080/courses/
-//Course: id, name, author
-    @RequestMapping("/courses")
-    public List<Course> retreiveAllCourses() {
-        return Arrays.asList(
-                new Course(1, "Learn AWS", "Aaron"),
-                new Course(2, "Learn DevOps", "in28minutes"),
-                new Course(3, "Learn React", "Josh")
-        );
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retreiveAllCourses() {
+        return configuration;
     }
 }
